ee7e0f5b1242fa356cc7c909c31e2cc029397b8e
nao-18 <kyahyo17@yahoo.co.jp>
2020-12-18 06:16:09 +0900

    add to vue-component.

--- sample-app/resources/js/app.js
+++ sample-app/resources/js/app.js
@@ -1,1 +1,12 @@
-require('./bootstrap');
+import Vue from 'vue'
+// Import the routing definition
+import router from './router'
+// Import the root component
+import App from './App.vue'
+
+new Vue({
+    el: '#app',
+    router, // Read the routing definition
+    components: { App }, // Declare the use of the root component
+    template: '<App />' // Draw the root component
+})
--- sample-app/webpack.mix.js
+++ sample-app/webpack.mix.js
@@ -11,8 +11,5 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.browserSync({
-    proxy: '0.0.0.0:8081', // アプリの起動アドレス
-    open: false // ブラウザを自動で開かない
-}).js('resources/js/app.js', 'public/js')
+mix.js('resources/js/app.js', 'public/js')
     .version()
