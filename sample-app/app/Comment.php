a23187cd3f1d8d4112924fe0041ecf171c94fda6
nao-18 <kyahyo17@yahoo.co.jp>
2020-12-29 14:35:39 +0900

    create comment-function.

---
+++ sample-app/app/Comment.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class Comment
+ * @package App
+ */
+class Comment extends Model
+{
+    /**
+     * @var array
+     */
+    protected $visible = [
+        'author', 'content',
+    ];
+
+    /**
+     * リレーションシップ - usersテーブル
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function author()
+    {
+        return $this->belongsTo('App\User', 'user_id', 'id', 'users');
+    }
+}
